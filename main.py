c9e836f43c512cbd1a9df8d3c5a6c528cee0c6c5
petayyyy <ilyaholodilov19072002@gmail.com>
2025-09-12 14:09:21 +0300

    Update output signal and web page

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
-from flask import Flask, render_template, jsonify, request
-import threading
+from flask import Flask, render_template, jsonify
 import time
+import threading
 from queue import Queue
 import rospy
 from cv_bridge import CvBridge
 from sensor_msgs.msg import Image
 from pyzbar import pyzbar
-import os
-from datetime import datetime
+
+# out system
+from clover.srv import SetLEDEffect
+import pigpio
 
 app = Flask(__name__)
 bridge = CvBridge()
+
+set_effect = rospy.ServiceProxy('led/set_effect', SetLEDEffect)  # define proxy to ROS-service
 
 # Глобальные переменные для хранения статуса и результатов
 qr_queue = Queue()
@@ -24,13 +28,84 @@ attempt_duration = 0    # Длительность попытки
 all_found_flag = False  # Флаг, что все QR-коды найдены
 manual_qr_counter = 0   # Счётчик для ручного добавления QR-кодов
 
+# out systems
+isLed = True
+isSound = False
+soundPin = 11
+pi = pigpio.pi()
+pi.set_mode(soundPin, pigpio.OUTPUT)
+
+def setFound():
+    global isLed, isSound, soundPin
+    if (not isLed and not isSound): return
+    if (isLed):
+        set_effect(effect='blink', r=0, g=255, b=0)
+    if (isSound):
+        pi.write(soundPin, 1)
+    
+    rospy.sleep(3)
+    
+    if (isLed):
+        set_effect(r=0, g=0, b=0)
+    if (isSound):
+        pi.write(soundPin, 0)
+
+def setEnd():
+    global isLed, isSound, soundPin
+    if (not isLed and not isSound): return
+
+    for i in range(5):
+        if (isLed):
+            if (i % 2 == 0): set_effect(r=255, g=0, b=0)
+            else: set_effect(r=0, g=0, b=255)
+        if (isSound):
+            pi.write(soundPin, 1)
+        rospy.sleep(1)
+        
+        if (isLed):
+            set_effect(r=0, g=0, b=0)
+        if (isSound):
+            pi.write(soundPin, 0)
+        rospy.sleep(1)
+
+def setQr(qr_data:str):
+    global detection_active, ros_initialized, found_qrs, attempt_active, all_found_flag
+    # Добавляем только новые уникальные QR-коды
+    if ("MANUAL_QR" in qr_data or (qr_data not in found_qrs)) and len(found_qrs) < max_qrs:
+        if (len(found_qrs) < max_qrs):
+            found_qrs.add(qr_data)
+            print(f"Обнаружен новый QR-код: {qr_data}")
+            # Добавляем в очередь
+            qr_queue.put(qr_data)
+            setFound()
+        elif (len(found_qrs) == max_qrs-1):
+            found_qrs.add(qr_data)
+            print("Все QR-коды найдены!")
+            all_found_flag = True
+            qr_queue.put("ALL_FOUND")
+            setEnd()
+
+def image_callback(data):
+    global detection_active, ros_initialized, found_qrs, attempt_active, all_found_flag
+    img = bridge.imgmsg_to_cv2(data, 'bgr8')
+    if (detection_active and attempt_active and not all_found_flag):
+        barcodes = pyzbar.decode(img)
+        
+        if barcodes:
+            for barcode in barcodes:
+                qr_data = barcode.data.decode('utf-8')
+                setQr(qr_data = qr_data)
+
 def init_ros():
+    global image_sub
     """Инициализация ROS в основном потоке"""
     global ros_initialized
     try:
         rospy.init_node('qr_detector', anonymous=True, disable_signals=True)
+        image_sub = rospy.Subscriber('main_camera/image_raw', Image, image_callback)
         ros_initialized = True
         print("ROS node initialized successfully")
+        # rospy.spin()
     except Exception as e:
         print(f"Failed to initialize ROS node: {e}")
 
@@ -175,18 +250,8 @@ def manual_add_qr():
     # Генерируем уникальный QR-код для ручного добавления
     manual_qr_counter += 1
     qr_data = f"MANUAL_QR_{manual_qr_counter}"
-    
-    # Добавляем QR-код
-    found_qrs.add(qr_data)
-    print(f"Ручное добавление QR-кода: {qr_data}")
-    qr_queue.put(qr_data)
-    
-    # Если найдены все QR-коды, останавливаем попытку
-    if len(found_qrs) == max_qrs:
-        print("Все QR-коды найдены!")
-        all_found_flag = True
-        qr_queue.put("ALL_FOUND")
-    
+    setQr(qr_data = qr_data)
+
     return jsonify({'status': 'success', 'message': f'Добавлен QR-код: {qr_data}'})
 
 @app.route('/static/<path:filename>')
@@ -196,11 +261,13 @@ def serve_static(filename):
 
 if __name__ == '__main__':
     # Инициализируем ROS в основном потоке
-    init_ros()
-    
-    # Запускаем детекцию QR-кодов в фоновом режиме
-    detection_thread = threading.Thread(target=qr_detection_loop)
-    detection_thread.daemon = True
-    detection_thread.start()
+    # init_ros()
+    ros_thread = threading.Thread(target=init_ros)
+    ros_thread.daemon = True
+    ros_thread.start()
+    # # Запускаем детекцию QR-кодов в фоновом режиме
+    # detection_thread = threading.Thread(target=qr_detection_loop)
+    # detection_thread.daemon = True
+    # detection_thread.start()
 
     app.run(host='0.0.0.0', port=5000, debug=False, use_reloader=False)
